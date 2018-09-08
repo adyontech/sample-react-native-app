1126f7cce9a33c83dc2d63f65dea6e1d5fbfb09f
aadii104 <sharmaadi104@gmail.com>
2018-09-08 13:13:23 +0530

    login screen done.

--- src/screens/AuthScreens/login/index.tsx
+++ src/screens/AuthScreens/login/index.tsx
@@ -15,21 +15,82 @@ import {
 } from "react-native";
 
 export default class Login extends Component {
+  // keyboardDidShowListener: any;
+  // keyboardDidHideListener: any;
+  // keyboardPresent: Boolean;
   constructor(props) {
+    super(props);
+    // this.keyboardPresent = false;
     super(props);
     this.state = {};
   }
+  // componentWillMount() {
+  //   this.keyboardDidShowListener = Keyboard.addListener(
+  //     "keyboardDidShow",
+  //     this._keyboardToggle(true)
+  //   );
+  //   this.keyboardDidHideListener = Keyboard.addListener(
+  //     "keyboardDidHide",
+  //     this._keyboardToggle(false)
+  //   );
+  // }
+
+  // componentWillUnmount() {
+  //   this.keyboardDidShowListener.remove();
+  //   this.keyboardDidHideListener.remove();
+  // }
+
+  // _keyboardToggle(val) {
+  //   this.keyboardPresent = val;
+  // }
   render() {
     return (
-      <View style={styles.container}>
-        <View style={styles.logoContainer}>
-          <Image
-            source={require("./../../../static/img/rnicon.png")}
-            style={styles.logo}
-          />
-          <Text style={styles.title}>Manage your placements.</Text>
-        </View>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <KeyboardAvoidingView style={styles.container}>
+          <TouchableWithoutFeedback
+            style={styles.container}
+            onPress={Keyboard.dismiss}
+          >
+            <View style={styles.logoContainer}>
+              <View style={styles.logoContainer}>
+                <Image
+                  source={require("./../../../static/img/rnicon.png")}
+                  style={styles.logo}
+                />
+                <Text style={styles.title}>Manage your placements.</Text>
+              </View>
+              <View style={styles.infoContainer}>
+                <TextInput
+                  style={styles.input}
+                  placeholder="Enter your email"
+                  placeholderTextColor="rgba(255,255,255, .8)"
+                  keyboardType="email-address"
+                  returnKeyType="next"
+                  autoCorrect={false}
+                  onSubmitEditing={() => this.refs.txtPassword.focus()}
+                />
+
+                <TextInput
+                  style={styles.input}
+                  placeholder="Enter password"
+                  placeholderTextColor="rgba(255,255,255, .8)"
+                  returnKeyType="go"
+                  autoCorrect={false}
+                  secureTextEntry
+                  ref={"txtPassword"}
+                />
+                <TouchableOpacity style={styles.buttonContainer}>
+                  <Text style={styles.buttonText}>Sign In</Text>
+                </TouchableOpacity>
+              </View>
+              <View style={styles.infoContainer}>
+                <Text style={styles.ForgotPassword}>Forgot password?</Text>
+              </View>
+            </View>
+          </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
+      </SafeAreaView>
     );
   }
 }
@@ -52,6 +113,39 @@ const styles = StyleSheet.create({
     color: "#f7c744",
     fontSize: 18,
     textAlign: "center",
-    marginTop: 15
+    marginTop: 5,
+    opacity: 0.9
+  },
+  infoContainer: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    bottom: 20,
+    height: 200,
+    padding: 20
+    // backgroundColor: "red"
+  },
+  input: {
+    height: 40,
+    backgroundColor: "rgba(255,255,255,0.2)",
+    color: "#fff",
+    marginBottom: 20,
+    paddingHorizontal: 10
+  },
+  buttonContainer: {
+    backgroundColor: "#f7c744",
+    paddingVertical: 15
+  },
+  buttonText: {
+    textAlign: "center",
+    fontWeight: "bold",
+    color: "yellow",
+    fontSize: 18
+  },
+  ForgotPassword: {
+    left: 0,
+    right: 0,
+    bottom: 20,
+    color: "yellow"
   }
 });
