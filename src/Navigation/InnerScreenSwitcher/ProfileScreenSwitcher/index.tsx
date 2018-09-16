d67751bdfd0f5c9ae23b24aba005e743448fdc11
aadii104 <sharmaadi104@gmail.com>
2018-09-16 12:11:45 +0530

    starting profile page.

---
+++ src/Navigation/InnerScreenSwitcher/ProfileScreenSwitcher/index.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import {
+  ActivityIndicator,
+  AsyncStorage,
+  Button,
+  StatusBar,
+  StyleSheet,
+  View
+} from "react-native";
+import { createStackNavigator, createSwitchNavigator } from "react-navigation";
+import { Animated, Easing } from "react-native";
+
+import MainProfile from "./../../../screens/Profile";
+
+const NewsFeedStack = createStackNavigator(
+  {
+    MainProfile: {
+      screen: MainProfile,
+      path: "feed"
+    }
+  },
+  {
+    headerMode: "none",
+    transitionConfig: () => ({
+      transitionSpec: {
+        duration: 0,
+        timing: Animated.timing,
+        easing: Easing.step0
+      }
+    })
+  }
+);
+export default createSwitchNavigator(
+  {
+    MainProfile
+  },
+  {
+    initialRouteName: "MainProfile"
+  },
+  {
+    headerMode: "none",
+    transitionConfig: () => ({
+      transitionSpec: {
+        duration: 0,
+        timing: Animated.timing,
+        easing: Easing.step0
+      }
+    })
+  }
+);
